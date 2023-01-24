4cb4d67017be667adc2f561fb4bb67c0bf9f22b4
qwe-kev <kevray39@gmail.com>
2023-01-24 17:35:52 +0530

    added login

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,10 @@ router.get('/signUp', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'index.html'))
 })
 
+router.get('/login', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'login.html'))
+})
+
 router.post('/createUser', (req, res, next) => {
     return User.create({
         name : req.body.name,
@@ -17,11 +21,36 @@ router.post('/createUser', (req, res, next) => {
         password : req.body.password
     })
     .then(user => {
+        //res.send(user);
         res.sendFile(path.join(rootDir, 'views', 'login.html'))
     })
     .catch(err => {
         console.log(err);
         res.sendFile(path.join(rootDir, 'views', 'signUpError.html'))
+    })
+})
+
+router.post('/users/login', (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log(email, password);
+    return User.findAll({
+       where : {
+        'email' : email,
+        'password' : password
+       }
+    })
+    .then((user) => {
+        console.log("user", user);
+        if(user.length > 0) {
+            res.send('<h1>Success<h1>')
+        }
+        else{
+            res.send('<h1>No user Found!!!</h1>')
+        }
+    })
+    .catch(err => {
+        console.log(err);
     })
 })
 
