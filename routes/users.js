bd3205dc0291e6d061204d1893e108866cea0d08
qwe-kev <kevray39@gmail.com>
2023-01-25 23:36:55 +0530

    Added bcrypt to login and signup

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,19 @@ const rootDir = path.dirname(require.main.filename);
 const users = [];
 const sequelize = require('../util/database');
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
+const hashPassword = async function(password) {
+    return await bcrypt.hash(password, saltRounds);
+}
+
+const checkUser = async function(password, hash) {
+    return new Promise((resolve, reject) => {
+        resolve(bcrypt.compare(password, hash));
+    })
+}
 
 router.get('/signUp', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'register.html'))
@@ -14,11 +27,16 @@ router.get('/login', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'login.html'))
 })
 
+
 router.post('/createUser', (req, res, next) => {
-    return User.create({
-        name : req.body.name,
-        email : req.body.email,
-        password : req.body.password
+    const password = req.body.password;
+    hashPassword(password)
+    .then((hashedPassword) => {
+        return User.create({
+            name : req.body.name,
+            email : req.body.email,
+            password : hashedPassword
+        })
     })
     .then(user => {
         res.status(200).redirect('/users/login');
@@ -38,20 +56,15 @@ router.post('/login', (req, res, next) => {
     })
     .then((user) => {
         if(user.length > 0) {
-            return User.findAll({
-                where : {
-                 'email' : email,
-                 'password' : password
-                }
-             })
+            return checkUser(password, user[0].dataValues.password);
         }
         else{
             res.json({status : 404, message : "User not found"});
         }
     })
-    .then(user => {
-        if(user.length > 0) {
-            res.json({status :200, message : "Successfully logged in", user})
+    .then(result => {
+        if(result) {
+            res.json({status :200, message : "Successfully logged in"})
         }
         else {
             res.json({status : 401, message : "User not authorized"});
